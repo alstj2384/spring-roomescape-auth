c49e0f78ba90220d0e264832c7a69463e9183bdb
alstj2384 <ass91160@naver.com>
2026-05-19 16:41:23 +0900

    refactor: DB 스키마에 Member 추가 및 반영

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,28 +1,39 @@
 DROP TABLE IF EXISTS reservation;
 DROP TABLE IF EXISTS reservation_time;
 DROP TABLE IF EXISTS theme;
+DROP TABLE IF EXISTS member;
+
+CREATE TABLE member (
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(20)  NOT NULL,
+    login_id VARCHAR(50)  NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY uk_member_login_id (login_id)
+);
 
 CREATE TABLE theme (
-    id      BIGINT      NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(20) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    thumbnail_url VARCHAR(255)  NOT NULL,
+    id            BIGINT       NOT NULL AUTO_INCREMENT,
+    name          VARCHAR(20)  NOT NULL,
+    description   VARCHAR(255) NOT NULL,
+    thumbnail_url VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE reservation_time (
-    id       BIGINT       NOT NULL AUTO_INCREMENT,
-    start_at TIME NOT NULL,
+    id       BIGINT NOT NULL AUTO_INCREMENT,
+    start_at TIME   NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE reservation (
-    id      BIGINT       NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(20) NOT NULL,
-    date    DATE NOT NULL,
-    time_id BIGINT NOT NULL,
-    theme_id BIGINT NOT NULL,
+    id       BIGINT      NOT NULL AUTO_INCREMENT,
+    member_id BIGINT     NOT NULL,
+    date     DATE        NOT NULL,
+    time_id  BIGINT      NOT NULL,
+    theme_id BIGINT      NOT NULL,
     PRIMARY KEY (id),
+    FOREIGN KEY (member_id) REFERENCES member (id) ON DELETE RESTRICT,
     FOREIGN KEY (time_id) REFERENCES reservation_time (id) ON DELETE RESTRICT,
     FOREIGN KEY (theme_id) REFERENCES theme (id) ON DELETE RESTRICT
 );
